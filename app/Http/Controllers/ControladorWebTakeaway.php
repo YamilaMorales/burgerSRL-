2f2341194ac2a9f443bfd10fbf7d7a8ee77d89e0
YamilaMorales <moralesyn@hotmail.com>
2022-11-06 13:10:19 -0300

    cambios

--- app/Http/Controllers/ControladorWebTakeaway.php
+++ app/Http/Controllers/ControladorWebTakeaway.php
@@ -1,66 +1,67 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Entidades\Sucursal;
 use App\Entidades\Producto;
 use App\Entidades\Carrito;
 use App\Entidades\Categoria;
 use Illuminate\Http\Request;
 use Session;
+
+require app_path() . '/start/constants.php';
 class ControladorWebTakeaway extends Controller
 {
     public function index()
     {
         $sucursal = new Sucursal();
-        $aSucursales = $sucursal->obtenerTodos();  
+        $aSucursales = $sucursal->obtenerTodos();
         $producto = new Producto();
-        $aProductos = $producto->obtenerTodos();  
+        $aProductos = $producto->obtenerTodos();
         $categoria = new Categoria();
-        $aCategorias= $categoria->obtenerTodos();  
-       
-        return view("web.takeaway", compact( "aSucursales" , "aProductos", "aCategorias"));
-           
+        $aCategorias = $categoria->obtenerTodos();
+
+        return view("web.takeaway", compact("aSucursales", "aProductos", "aCategorias"));
     }
 
 
-    public function insertar( Request $request){   
-    
-      $idCliente = Session::get("idCliente");
-      $idProducto = $request->input("txtProducto");
-      $cantidad=$request->input("txtCantidad");
-      $sucursal = new Sucursal();
-      $aSucursales = $sucursal->obtenerTodos();  
-      $categoria = new Categoria();
-      $aCategorias= $categoria->obtenerTodos(); 
-      $producto = new Producto();
-      $aProductos = $producto->obtenerTodos();  
-    
-        if(isset($idCliente) && $idCliente > 0 ){
+    public function insertar(Request $request)
+    {
 
-             if(isset($cantidad) && $cantidad > 0){
-             //se agrega al carrito y da un msj
-             $carrito = new Carrito();
-             $carrito->fk_idcliente = $idCliente;
-             $carrito->fk_idproducto = $idProducto;
+        $idCliente = Session::get("idCliente");
+        $idProducto = $request->input("txtProducto");
+        $cantidad = $request->input("txtCantidad");
+        $sucursal = new Sucursal();
+        $aSucursales = $sucursal->obtenerTodos();
+        $categoria = new Categoria();
+        $aCategorias = $categoria->obtenerTodos();
+        $producto = new Producto();
+        $aProductos = $producto->obtenerTodos();
 
-             $msg["ESTADO"] = MSG_SUCCESS;
-             $msg["MSG"] = "El producto se agrego al carrito.";
-             return view ('web.takeaway' , compact('msg',"aCategorias", "aSucursales","aProductos"));
 
-             } else {
-            //msj 
-            $msg["ESTADO"] = MSG_ERROR;
-            $msg["MSG"] = "El producto no se agrego al carrito.";
-            return view ('web.takeaway' , compact('msg' , "aCategorias" , "aSucursales","aProductos"));
+        if (isset($idCliente) && $idCliente > 0) {
+
+            if (isset($cantidad) && $cantidad > 0) {
+                //se agrega al carrito y da un msj
+                $carrito = new Carrito();
+                $carrito->fk_idcliente = $idCliente;
+                $carrito->fk_idproducto = $idProducto;
+                $carrito->cantidad = $cantidad;
+                $carrito->insertar();
+
+                $msg["ESTADO"] = MSG_SUCCESS;
+                $msg["MSG"] = "El producto se agrego al carrito.";
+                return view('web.takeaway', compact('msg', "aCategorias", "aSucursales", "aProductos"));
+            } else {
+                //msj 
+                $msg["ESTADO"] = MSG_ERROR;
+                $msg["MSG"] = "El producto no se agrego al carrito.";
+                return view('web.takeaway', compact('msg', "aCategorias", "aSucursales", "aProductos"));
             }
-
-            } else { 
+        } else {
             $msg["ESTADO"] = MSG_ERROR;
             $msg["MSG"] = "Debe iniciar sesion para realizar un pedido.";
-             return view ('web.takeaway' , compact('msg', "aCategorias", "aSucursales","aProductos"));
-
-           }
+            return view('web.login', compact('msg', "aCategorias", "aSucursales", "aProductos"));
         }
-
     }
-
+}
